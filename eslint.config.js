4b89bc4aa797e02f666c706999c9e149f46da630
Chad Oakenfold <web@oakenfold.ca>
2025-09-17 08:51:32 -0400

    feat: adds eslint

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import js from "@eslint/js";
+import astroPlugin from "eslint-plugin-astro";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  js.configs.recommended,
+  ...astroPlugin.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,tsx,astro}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      // Catch undefined variables
+      "no-undef": "error",
+      "no-unused-vars": "error",
+      "@typescript-eslint/no-unused-vars": "error",
+
+      // Additional helpful rules
+      "no-console": "warn",
+      "prefer-const": "error",
+      "no-var": "error",
+    },
+  },
+  {
+    files: ["**/*.astro"],
+    languageOptions: {
+      parser: astroPlugin.parser,
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+    },
+  },
+];
